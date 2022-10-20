76e7bb4cba995e0e0d0299fbfe2bcb771cc2f95e
Cheikhna-sakho <cheikh.bsakho@gmail.com>
2022-10-20 17:32:06 +0200

    font-color

--- src/components/about/contanct/ContactForm.js
+++ src/components/about/contanct/ContactForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ContactForm = () => {
-    const champs = [{ id: "username", title: "Non/prenom" }, { id: "email", title: "E-mail" }];
+    const champs = [{ id: "username",type:"text", title: "Nom/prenom" }, { id: "email",type:"email", title: "E-mail" }];
     return (
         <article className='contact' id='contact'>
             <div className='contact-title'>
@@ -10,15 +10,15 @@ const ContactForm = () => {
             <div className='form-box grid-row'>
                 <form className='info-artilce'>
                     <div className="input-group">
-                        {champs.map((nameInput,i) => (
+                        {champs.map(({title,type,id},i) => (
                             <p className="input-box grid-row" key={i}>
-                                <label htmlFor="">{nameInput.title}</label>
-                                <input type="text" name={nameInput.id} />
+                                <label htmlFor={id}>{title}</label>
+                                <input type={type} name={id} id={id} required />
                             </p>
                         ))}
                     </div>
                     <p>
-                        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                        <textarea name="message" placeholder='Veillez saisir votre message' id="message" cols="30" rows="10" required/>
                     </p>
                     <input type="submit" value="Envoyez" />
                 </form>
